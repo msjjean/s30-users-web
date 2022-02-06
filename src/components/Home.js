0702b48f65014c8f47917cf43c3bd23c68c7bb63
msjjean <jorencejean@gmail.com>
2022-02-06 22:34:12 +0800

    fix indentions

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 import UserIndex from './users/UserIndex';
 
 const Home = () => {
-	const [users, setUsers] = useState([]);
-	useEffect( () => {
-		axios.get('http://127.0.0.1:8000/api/users')
-			.then(function ({ data }) {
-				setUsers(data.users);
-			})
-			.catch(function (error) {
-				console.log(error);
-			})
-			.then(function () {
-					//console.log('always exec');
-			});  
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    axios.get('http://127.0.0.1:8000/api/users')
+      .then(function ({ data }) {
+        setUsers(data.users);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        //console.log('always exec');
+      });  
 	}, []);
 
 	return ( 
-		<UserIndex users={users} />
+    <UserIndex users={users} />
 	);
 }
  
