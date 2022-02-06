4f8fb73ecec41e9a15147a50bdcf2d0ec54e9ec9
msjjean <jorencejean@gmail.com>
2022-02-06 21:41:46 +0800

    create new user form

--- src/components/users/NewUser.js
+++ src/components/users/NewUser.js
@@ -1,6 +1,53 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
 const NewUser = () => {
+    const [name, setName]   = useState('');
+    const [email, setEmail] = useState('');
+   
+    const navigate = useNavigate();
+    
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        axios.post('http://127.0.0.1:8000/api/users', {
+            fullname: name, 
+            email: email 
+        })
+        .then(function (response) {
+            navigate('/');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     return (
-        <h1> New User </h1>
+        <div>
+            <h1 className="page-title"> Create New User </h1>
+            <form className="user-form" onSubmit={submitForm}>
+                <label>Full Name:</label>
+                <input 
+                    type="text" 
+                    required 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <label>Email</label>
+                <input
+                    type="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                
+                <div className="btn-div">
+                    <button>Add User</button>
+                </div>
+            </form>
+        </div>
     );
 }
  
